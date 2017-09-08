ecdb25718e11a679151c158ea9206c8aa660ff70
Branden Allen <ballen@cfa.harvard.edu>
2017-09-08 13:47:10 -0400

    Addition of a reference (Old Uncommited Change)

--- bin/L14/summary.py
+++ bin/L14/summary.py
@@ -5,6 +5,10 @@ Summary Plots and Test Plotting Procedures for L+14 Data
 
 Author: Branden Allen
 Date: 2016.10.07
+
+References
+----------
+[1] https://ngdc.noaa.gov/stp/satellite/goes/doc/GOES_XRS_readme.pdf
 '''
 from __future__ import print_function
 
@@ -32,7 +36,7 @@ def read_csv(filename):
 		idx= [i.end() for i in re.finditer('data:',buf)]
 		d= pa.read_csv(sio.StringIO(buf[idx[0]:]))
 
-		#Quality Flag Filter and Flux Factor Application
+		#Quality Flag Filter and SWPC Flux Factor Removal For True Fluxes [1]
 		d= d[d['A_QUAL_FLAG']&d['B_QUAL_FLAG']== 0]
 		d['A_FLUX']/= 0.85
 		d['B_FLUX']/= 0.7
@@ -54,13 +58,38 @@ def read_csv(filename):
 		d['Time [UT]']= d['time_tag'].apply(pa.Timestamp)
 		return d.set_index('Time [UT]')
 
-#Extract data and plot the GOES derived temperatures for all monitors
+#Extract data and re-arrange axes for efficient plotting
 base_name= '_xrs_2s_20160921_20160921.csv'
 data_dir= 'data/GOES_full'
 data= dict([('g'+repr(i),read_csv(os.path.join(data_dir,'g'+repr(i)+base_name))) for i in xrange(13,16)])
 data= pa.concat(data, 1)
 data= data.swaplevel(0,1,1)
+
+#Temperature Plot
 data['T [MK]'].plot(ls= 'none', marker= '.')
+#pl.xlim(['2016-09-21 12:30:00','2016-09-21 14:30:00'])
+pl.xlim(['2016-09-21 16:00:00','2016-09-21 19:00:00'])
+pl.ylabel('Temperature [MK]')
+pl.minorticks_on()
+
+#Emission Measure Plot
+data['Emission Measure [1/cm2]'].plot(ls= 'none', marker= '.')
+#pl.xlim(['2016-09-21 12:30:00','2016-09-21 14:30:00'])
+#pl.xlim(['2016-09-21 04:00:00','2016-09-21 06:00:00'])
+pl.xlim(['2016-09-21 16:00:00','2016-09-21 19:00:00'])
+pl.ylabel('Emission Measure [1/cm2]')
+pl.semilogy()
+pl.minorticks_on()
+
+#Raw Observed Flux Plot
+data[['A_FLUX','B_FLUX']].plot(ls= 'None', marker= '.')
+#pl.xlim(['2016-09-21 12:30:00','2016-09-21 14:30:00'])
+#pl.xlim(['2016-09-21 04:00:00','2016-09-21 06:00:00'])
+pl.xlim(['2016-09-21 16:00:00','2016-09-21 19:00:00'])
+pl.ylabel('Incident Flux [W/m2]')
+pl.semilogy()
+pl.ylim([5e-10,1e-2])
+pl.minorticks_on()
 
 #####################################################################
 
